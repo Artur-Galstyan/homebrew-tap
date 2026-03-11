ac3e3c7d469040c0754e438a5519ed206b12eeca
Artur Galstyan <galstyan.artu@gmail.com>
2026-03-11 17:03:11 +0100

    update

--- immemor.rb
+++ immemor.rb
@@ -1,12 +1,11 @@
-# Casks/immemor.rb
-  cask "immemor" do
-    version "1.0.0"
-    sha256 "SHA256_OF_DMG"
+cask "immemor" do
+  version "0.1.0"
+  sha256 "f530acc0217389c0fe8e86fbefe3ce7ef556ba759db25fe585a35cc6092f9668"
 
-    url "https://github.com/arturgalstyan/immemor/releases/download/v#{version}/immemor_#{version}_universal.dmg"
-    name "immemor"
-    desc "Spaced repetition flashcard app"
-    homepage "https://immemor.app"
+  url "https://github.com/Artur-Galstyan/immemor/releases/download/v#{version}/immemor_#{version}_universal.dmg"
+  name "immemor"
+  desc "Spaced repetition flashcard app"
+  homepage "https://immemor.app"
 
-    app "immemor.app"
-  end
+  app "immemor.app"
+end
